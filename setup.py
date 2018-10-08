c7c906183bc3005232e6661386590b6985f98a14
Alex Moneger <alexmgr+github@gmail.com>
2018-10-08 16:08:39 -0700

    Added nested pdb folder to setup.py
    - Removed a dead symlink
    - Initially forgot to add pdb nested lib to setup.py packages.
      Fixed that

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 setuptools.setup(
     name='pdbfile',
     version='0.0.1',
-    packages=['pdbfile'],
+    packages=['pdbfile', 'pdbfile.pdb'],
     description='A basic clone of the Microsoft clr PDB file parser (debug symbols)',
     author='Mountainstorm',
     install_requires=['pefile'],
